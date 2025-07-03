74d62738828283683029606235af60ae412c601c
Thomas Remmery <thomas.remmery@skyline.be>
2025-07-03 08:27:33 +0200

    Add Connector Name field and overload constructor to initialize LockManagerElement based on element Name.

--- LockManager/LockManagerElement.cs
+++ LockManager/LockManagerElement.cs
@@ -1,192 +1,217 @@
 ﻿namespace Skyline.DataMiner.ConnectorAPI.SkylineLockManager.LockManager
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics;
-	using System.Linq;
-	using Newtonsoft.Json;
-	using Skyline.DataMiner.ConnectorAPI.SkylineLockManager;
-	using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages;
-	using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages.Locking;
-	using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages.Unlocking;
-	using Skyline.DataMiner.Core.DataMinerSystem.Common;
-	using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
-	using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
-	using Skyline.DataMiner.Net;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using Skyline.DataMiner.ConnectorAPI.SkylineLockManager;
+    using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages;
+    using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages.Locking;
+    using Skyline.DataMiner.ConnectorAPI.SkylineLockManager.Messages.Unlocking;
+    using Skyline.DataMiner.Core.DataMinerSystem.Common;
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+    using Skyline.DataMiner.Net;
 
-	/// <inheritdoc cref="ILockManagerElement"/>
-	public class LockManagerElement : ILockManagerElement
-	{
-		private static readonly int InterAppReceive_ParameterId = 9000000;
-		private static readonly int InterAppTimeout_ParameterId = 100;
+    /// <inheritdoc cref="ILockManagerElement"/>
+    public class LockManagerElement : ILockManagerElement
+    {
+        private static readonly int InterAppReceive_ParameterId = 9000000;
+        private static readonly int InterAppTimeout_ParameterId = 100;
 
-		private readonly IConnection connection;
-		private readonly IDmsElement element;
-		private readonly ILogger logger;
+        private readonly IConnection connection;
+        private readonly IDmsElement element;
+        private readonly ILogger logger;
 
-		private readonly Lazy<TimeSpan> timeout;
+        private readonly Lazy<TimeSpan> timeout;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="LockManagerElement"/> class.
-		/// </summary>
-		/// <param name="connection">Connection used to communicate with the Lock Manager element.</param>
-		/// <param name="agentId">ID of the agent on which the Lock Manager element is hosted.</param>
-		/// <param name="elementId">ID of the Lock Manager element.</param>
-		/// <param name="logger">Object that is used to log info about locks.</param>
-		/// <exception cref="ArgumentNullException">Thrown when the provided connection or the element is null.</exception>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown when provided element id or agent id is negative.</exception>
-		/// <exception cref="InvalidOperationException">Thrown when described element is inactive.</exception>
-		public LockManagerElement(IConnection connection, int agentId, int elementId, ILogger logger = null)
-		{
-			this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
-			this.logger = logger;
-			this.timeout = new Lazy<TimeSpan>(GetTimeout);
+        /// <summary>
+        /// Name of the Connector with which this element is able to communicate.
+        /// </summary>
+        public static readonly string SkylineLockManager_ConnectorName = "Skyline Lock Manager";
 
-			element = connection.GetDms().GetElement(new DmsElementId(agentId, elementId)) ?? throw new ArgumentException($"Unable to find an element with ID {agentId}\\{elementId}", nameof(elementId));
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LockManagerElement"/> class.
+        /// </summary>
+        /// <param name="connection">Connection used to communicate with the Lock Manager element.</param>
+        /// <param name="agentId">ID of the agent on which the Lock Manager element is hosted.</param>
+        /// <param name="elementId">ID of the Lock Manager element.</param>
+        /// <param name="logger">Object that is used to log info about locks.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the provided connection or the element is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when provided element id or agent id is negative.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when described element is inactive.</exception>
+        public LockManagerElement(IConnection connection, int agentId, int elementId, ILogger logger = null)
+        {
+            this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            this.logger = logger;
+            this.timeout = new Lazy<TimeSpan>(GetTimeout);
 
-			if (element.State != ElementState.Active) throw new InvalidOperationException($"Element {element.Name} is not active");
-		}
+            element = connection.GetDms().GetElement(new DmsElementId(agentId, elementId)) ?? throw new ArgumentException($"Unable to find an element with ID {agentId}\\{elementId}", nameof(elementId));
 
-		private TimeSpan Timeout => timeout.Value;
+            if (element.State != ElementState.Active) throw new InvalidOperationException($"Element {element.Name} is not active");
+        }
 
-		/// <summary>
-		/// A collection containing all classes used in InterApp communication.
-		/// </summary>
-		public static IReadOnlyCollection<Type> InterAppKnownTypes { get; } = new List<Type>
-		{
-			typeof(IInterAppCall),
-			typeof(LockObjectsRequestsMessage),
-			typeof(LockObjectsResponsesMessage),
-			typeof(UnlockObjectsRequestsMessage),
-			typeof(FailureMessage),
-		};
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LockManagerElement"/> class.
+        /// </summary>
+        /// <param name="connection">Connection used to communicate with the Lock Manager element.</param>
+        /// <param name="elementName">Name of the Lock Manager element.</param>
+        /// <param name="logger">Object that is used to log info about locks.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the provided connection or the element is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when provided element id or agent id is negative.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when described element is inactive.</exception>
+        public LockManagerElement(IConnection connection, string elementName, ILogger logger = null)
+        {
+            this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            this.logger = logger;
+            this.timeout = new Lazy<TimeSpan>(GetTimeout);
 
-		/// <inheritdoc cref="ILockManagerElement.LockObject(LockObjectRequest)"/>
-		/// <exception cref="ArgumentNullException"/>
-		/// <exception cref="InvalidOperationException"/>
-		public ILockInfo LockObject(LockObjectRequest request)
-		{
-			return LockObjects(new[] { request }).Single();
-		}
+            element = connection.GetDms().GetElement(elementName) ?? throw new ArgumentException($"Unable to find an element with Name {elementName}", nameof(elementName));
 
-		/// <inheritdoc cref="ILockManagerElement.LockObjects(IEnumerable{LockObjectRequest})"/>
-		/// <exception cref="ArgumentNullException"/>
-		/// <exception cref="InvalidOperationException"/>
-		public IEnumerable<ILockInfo> LockObjects(IEnumerable<LockObjectRequest> requests)
-		{
-			if (requests is null) throw new ArgumentNullException(nameof(requests));
+            if (element.State != ElementState.Active) throw new InvalidOperationException($"Element {element.Name} is not active");
+        }
 
-			var requestsList = requests.ToList();
+        private TimeSpan Timeout => timeout.Value;
 
-			if (!requestsList.Any()) return Enumerable.Empty<ILockInfo>();
+        /// <summary>
+        /// A collection containing all classes used in InterApp communication.
+        /// </summary>
+        public static IReadOnlyCollection<Type> InterAppKnownTypes { get; } = new List<Type>
+        {
+            typeof(IInterAppCall),
+            typeof(LockObjectsRequestsMessage),
+            typeof(LockObjectsResponsesMessage),
+            typeof(UnlockObjectsRequestsMessage),
+            typeof(FailureMessage),
+        };
 
-			var lockObjectsRequestsMessage = new LockObjectsRequestsMessage
-			{
-				Requests = requestsList,
-			};
+        /// <inheritdoc cref="ILockManagerElement.LockObject(LockObjectRequest)"/>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="InvalidOperationException"/>
+        public ILockInfo LockObject(LockObjectRequest request)
+        {
+            return LockObjects(new[] { request }).Single();
+        }
 
-			Log($"Requesting locks for {string.Join(", ", requestsList)}");
+        /// <inheritdoc cref="ILockManagerElement.LockObjects(IEnumerable{LockObjectRequest})"/>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="InvalidOperationException"/>
+        public IEnumerable<ILockInfo> LockObjects(IEnumerable<LockObjectRequest> requests)
+        {
+            if (requests is null) throw new ArgumentNullException(nameof(requests));
 
-			SendMessageWithResponse(lockObjectsRequestsMessage, out LockObjectsResponsesMessage responseMessage);
+            var requestsList = requests.ToList();
 
-			return responseMessage.Responses.Select(x => new LockInfo
-			{
-				LockHolderInfo = x.LockHolderInfo,
-				IsGranted = x.LockIsGranted,
-				ObjectId = x.ObjectId,
-				AutoUnlockTimestamp = x.AutoUnlockTimestamp,
-			}).ToList();
-		}
+            if (!requestsList.Any()) return Enumerable.Empty<ILockInfo>();
 
-		/// <inheritdoc cref="ILockManagerElement.UnlockObject(UnlockObjectRequest)"/>
-		/// <exception cref="ArgumentNullException"/>
-		public void UnlockObject(UnlockObjectRequest request)
-		{
-			UnlockObjects(new[] { request });
-		}
+            var lockObjectsRequestsMessage = new LockObjectsRequestsMessage
+            {
+                Requests = requestsList,
+            };
 
-		/// <inheritdoc cref="ILockManagerElement.UnlockObjects(IEnumerable{UnlockObjectRequest})"/>
-		/// <exception cref="ArgumentNullException"/>
-		public void UnlockObjects(IEnumerable<UnlockObjectRequest> requests)
-		{
-			if (requests is null) throw new ArgumentNullException(nameof(requests));
+            Log($"Requesting locks for {string.Join(", ", requestsList)}");
 
-			var requestsList = requests.ToList();
+            SendMessageWithResponse(lockObjectsRequestsMessage, out LockObjectsResponsesMessage responseMessage);
 
-			if (!requestsList.Any()) return;
+            return responseMessage.Responses.Select(x => new LockInfo
+            {
+                LockHolderInfo = x.LockHolderInfo,
+                IsGranted = x.LockIsGranted,
+                ObjectId = x.ObjectId,
+                AutoUnlockTimestamp = x.AutoUnlockTimestamp,
+            }).ToList();
+        }
 
-			var message = new UnlockObjectsRequestsMessage
-			{
-				Requests = requestsList,
-			};
+        /// <inheritdoc cref="ILockManagerElement.UnlockObject(UnlockObjectRequest)"/>
+        /// <exception cref="ArgumentNullException"/>
+        public void UnlockObject(UnlockObjectRequest request)
+        {
+            UnlockObjects(new[] { request });
+        }
 
-			SendMessageWithoutResponse(message);
+        /// <inheritdoc cref="ILockManagerElement.UnlockObjects(IEnumerable{UnlockObjectRequest})"/>
+        /// <exception cref="ArgumentNullException"/>
+        public void UnlockObjects(IEnumerable<UnlockObjectRequest> requests)
+        {
+            if (requests is null) throw new ArgumentNullException(nameof(requests));
 
-			Log($"Releasing locks for {string.Join(", ", requestsList)}");
-		}
+            var requestsList = requests.ToList();
 
-		private void SendMessageWithoutResponse(Message message)
-		{
-			var commands = InterAppCallFactory.CreateNew();
-			commands.Messages.Add(message);
+            if (!requestsList.Any()) return;
 
-			commands.Send(connection, element.AgentId, element.Id, InterAppReceive_ParameterId, InterAppKnownTypes);
-		}
+            var message = new UnlockObjectsRequestsMessage
+            {
+                Requests = requestsList,
+            };
 
-		private void SendMessageWithResponse<T>(Message message, out T responseMessage) where T : Message
-		{
-			responseMessage = default;
+            SendMessageWithoutResponse(message);
 
-			var commands = InterAppCallFactory.CreateNew();
-			commands.Messages.Add(message);
+            Log($"Releasing locks for {string.Join(", ", requestsList)}");
+        }
 
-			Log($"Message: {JsonConvert.SerializeObject(message)}");
+        private void SendMessageWithoutResponse(Message message)
+        {
+            var commands = InterAppCallFactory.CreateNew();
+            commands.Messages.Add(message);
 
-			var response = commands.Send(connection, element.AgentId, element.Id, InterAppReceive_ParameterId, Timeout, InterAppKnownTypes).First();
+            commands.Send(connection, element.AgentId, element.Id, InterAppReceive_ParameterId, InterAppKnownTypes);
+        }
 
-			Log($"Response: {JsonConvert.SerializeObject(response)}");
+        private void SendMessageWithResponse<T>(Message message, out T responseMessage) where T : Message
+        {
+            responseMessage = default;
 
-			if (response is FailureMessage failureResponse)
-			{
-				throw new InvalidOperationException(failureResponse.Message);
-			}
-			else if (response is T expectedResponse)
-			{
-				responseMessage = expectedResponse;
-			}
-			else
-			{
-				throw new InvalidOperationException($"Received response is not of type {typeof(T)}");
-			}	
-		}
+            var commands = InterAppCallFactory.CreateNew();
+            commands.Messages.Add(message);
 
-		private TimeSpan GetTimeout()
-		{
-			try
-			{
-				var timeoutInSeconds = element.GetStandaloneParameter<double?>(InterAppTimeout_ParameterId) ?? throw new ParameterNotFoundException($"Unable to find parameter {InterAppTimeout_ParameterId} in element {element.Name} ({element.DmsElementId})");
+            Log($"Message: {JsonConvert.SerializeObject(message)}");
 
-				var retrievedTimeout = TimeSpan.FromSeconds(timeoutInSeconds.GetValue().Value);
+            var response = commands.Send(connection, element.AgentId, element.Id, InterAppReceive_ParameterId, Timeout, InterAppKnownTypes).First();
 
-				Log($"Timeout: {timeout}");
+            Log($"Response: {JsonConvert.SerializeObject(response)}");
 
-				return retrievedTimeout;
-			}
-			catch (Exception e)
-			{
-				var retrievedTimeout = TimeSpan.FromSeconds(30);
+            if (response is FailureMessage failureResponse)
+            {
+                throw new InvalidOperationException(failureResponse.Message);
+            }
+            else if (response is T expectedResponse)
+            {
+                responseMessage = expectedResponse;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Received response is not of type {typeof(T)}");
+            }
+        }
 
-				Log($"Unable to retrieve timeout due to: {e}");
+        private TimeSpan GetTimeout()
+        {
+            try
+            {
+                var timeoutInSeconds = element.GetStandaloneParameter<double?>(InterAppTimeout_ParameterId) ?? throw new ParameterNotFoundException($"Unable to find parameter {InterAppTimeout_ParameterId} in element {element.Name} ({element.DmsElementId})");
 
-				return retrievedTimeout;
-			}
-		}
+                var retrievedTimeout = TimeSpan.FromSeconds(timeoutInSeconds.GetValue().Value);
 
-		private void Log(string message)
-		{
-			string nameOfMethod = new StackTrace().GetFrame(1).GetMethod().Name;
+                Log($"Timeout: {timeout}");
 
-			logger?.Log(nameof(LockManagerElement), nameOfMethod, message);
-		}
-	}
+                return retrievedTimeout;
+            }
+            catch (Exception e)
+            {
+                var retrievedTimeout = TimeSpan.FromSeconds(30);
+
+                Log($"Unable to retrieve timeout due to: {e}");
+
+                return retrievedTimeout;
+            }
+        }
+
+        private void Log(string message)
+        {
+            string nameOfMethod = new StackTrace().GetFrame(1).GetMethod().Name;
+
+            logger?.Log(nameof(LockManagerElement), nameOfMethod, message);
+        }
+    }
 }
